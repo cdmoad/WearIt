3dd37de05c5571e57b2e6e7d1ad929cd5727ebb2
Moad Hrimech <moadehrimech22@gmail.com>
2023-09-15 18:48:58 +0200

    add animation to text and buttons

--- myapp/src/components/banner/banner.jsx
+++ myapp/src/components/banner/banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './banner.css';
+import {motion} from "framer-motion"
  
 function Banner({title1,title2,text}) {
   return (
@@ -15,32 +16,41 @@ function Banner({title1,title2,text}) {
     class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
   >
     <div class="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
-      <h1 class="text-3xl text-white font-extrabold sm:text-5xl">
-        {title1}
-
+      <motion.h1 class="text-3xl text-white font-extrabold sm:text-5xl " initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2 }}
+ >
+          {title1}
         <strong class="block font-extrabold text-custom-black white-text-shadow">
           {title2}
         </strong>
-      </h1>
+      </motion.h1>
 
       <p class="mt-4 max-w-lg sm:text-xl/relaxed text-white black-text-shadow">
          {text}
       </p>
 
       <div class="mt-8 flex flex-wrap gap-4 text-center">
-        <a
+        <motion.a
           href="#"
-          class="block w-full rounded bg-custom-black px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+          class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-custom-black shadow hover:bg-custom-black focus:outline-none focus:ring active:bg-rose-500 hover:text-white sm:w-auto"
+          initial={{y:"100px"}}
+          animate={{y:"0"}}
+          transition={{duration:1.3}}
+          
         >
           Explore Collections
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
           href="#"
-          class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-custom-black shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          class="block w-full rounded bg-custom-black px-12 py-3 text-sm font-medium text-white shadow hover:bg-white hover:text-custom-black focus:outline-none focus:ring active:text-custom-black sm:w-auto"
+          initial={{y:"100px"}}
+          animate={{y:"0"}}
+          transition={{duration:1.3}}
         >
           Learn More
-        </a>
+        </motion.a>
       </div>
     </div>
   </div>

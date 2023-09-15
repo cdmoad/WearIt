9f2abdebb19bd9e489b9697586994a13c3e8d21a
cdmoad <moadehrimech22@gmail.com>
2023-09-15 22:16:57 +0100

    add collection in component in home page

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -5,6 +5,8 @@ import Banner from './components/banner/banner'
 import Cta from './components/cta/cta'
 import CardSection from './components/cardSection/cardSection'
 import Footer from './layouts/footer'
+import Collection from './components/collection/collection'
+import Home from './pages/home'
  
 
 function App() {
@@ -12,12 +14,7 @@ function App() {
   return (
     <>
 
-    <Header/>
-    <Banner title1="WearIt" title2="Shop the Latest Trends" text="Welcome to our fashion store, where style meets comfort! Explore our collection, from classics to trends. Discover clothing that looks great and feels amazing. Start your style journey today!" />
-    <Cta/>
-    <CardSection/>
-    <Footer/>
-      
+   <Home/>
     </>
   )
 }
